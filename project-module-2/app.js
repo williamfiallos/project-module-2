58d2c073f73c9a4434fcdc5af690d57c54af9f18
williamfiallos <wfiallos10@gmail.com>
2019-03-06 19:24:23 -0500

    update dotenv file with variables

--- project-module-2/app.js
+++ project-module-2/app.js
@@ -23,7 +23,8 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 
 mongoose
-  .connect('mongodb://localhost/project-module-2', {useNewUrlParser: true})
+  // .connect('mongodb://localhost/project-module-2', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
